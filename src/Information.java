c545a3e8561e53a63c8ba7013d1cc2ed725c1e9c
jleal123 <140639277+jleal123@users.noreply.github.com>
2023-07-29 11:32:26 -0400

    Update Information.java

--- src/Information.java
+++ src/Information.java
@@ -4,7 +4,7 @@ public class Information {
 	public static void main(String[] args) {
 
 		System.out.println("My name is Josh. I am 30 years old");
-
+	}
 		public static void add(int a, int b){
 					System.out.println(a+b);
 
